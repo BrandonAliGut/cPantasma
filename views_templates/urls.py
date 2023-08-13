af180d427000fde5259c1b1a0f73807fa607e823
Brandon <bg4710699@gmail.com>
2023-08-13 10:13:47 -0600

    django udate11

--- views_templates/urls.py
+++ views_templates/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from .view_index import index, Admin_page_home,IndexRegister,contribuyente,Estado_Contribuyente,trabajo_type_pagos
 #index home
 urlpatterns = [
-    path(" ",index, name="home"),
+    path("",index, name="home"),
     path("Home_page/", Admin_page_home.as_view(), name="home_grupo"),
     path("Home_Register/", IndexRegister, name="home_admin"),
     path("SuperAdmin", superAdmin, name="admin_contabilidad"),
