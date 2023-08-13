bb716a0d76b0093bd1f31d99b4fa731a05e83672
Brandon <bg4710699@gmail.com>
2023-08-13 09:19:27 -0600

    django udate8

--- Registro/settings.py
+++ Registro/settings.py
@@ -38,10 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    'User',
-    'views_templates',
-    'Base_register',
+    'Registro.app.UserConfig',
+    'Registro.app.ViewsTemplatesConfig',
+    'Registro.app.BaseRegisterConfig',
 ]
 
 MIDDLEWARE = [
