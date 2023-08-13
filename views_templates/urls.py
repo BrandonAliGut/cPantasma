5962d61eef1f1037e313435d1bda3be4308d255c
Brandon <bg4710699@gmail.com>
2023-08-13 09:32:06 -0600

    django udate10

--- views_templates/urls.py
+++ views_templates/urls.py
@@ -6,13 +6,13 @@ from django.conf import settings
 from .view_index import index, Admin_page_home,IndexRegister,contribuyente,Estado_Contribuyente,trabajo_type_pagos
 #index home
 urlpatterns = [
-    path("/",index, name="home"),
-    path("/Home_page", Admin_page_home.as_view(), name="home_grupo"),
-    path("/Home_Register", IndexRegister, name="home_admin"),
-    path("/SuperAdmin", superAdmin, name="admin_contabilidad"),
-    path("/add_contribu/<str:typeaction>/<str:pk>", contribuyente, name="add_contribuyente"),
-    path("/EstadoContri", Estado_Contribuyente, name="EstadoContri"),
-    path("/add_trabajos/<str:typeaction>/<str:pk>/<str:add>", trabajo_type_pagos, name="add_trabajo"),
+    path(" ",index, name="home"),
+    path("Home_page/", Admin_page_home.as_view(), name="home_grupo"),
+    path("Home_Register/", IndexRegister, name="home_admin"),
+    path("SuperAdmin", superAdmin, name="admin_contabilidad"),
+    path("add_contribu/<str:typeaction>/<str:pk>", contribuyente, name="add_contribuyente"),
+    path("EstadoContri", Estado_Contribuyente, name="EstadoContri"),
+    path("add_trabajos/<str:typeaction>/<str:pk>/<str:add>", trabajo_type_pagos, name="add_trabajo"),
     
     
     
